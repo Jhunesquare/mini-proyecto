17db21e23b95ab6a7eead3f13321b2d0a8bfc13b
VictorSoto <victor.soto01@uceva.edu.co>
2024-04-19 21:08:15 -0500

    Terminado
    se agregó el html cosrrespondiente y las funciones al app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mini_proyecto';
+  lineas: number = 0;
+  mensaje = '';
+  resultado = '';
+
+  cifrarMensaje(): void {
+    let resultado = '';
+    for (let i = 0; i < this.lineas; i++) {
+      for (let j = i; j < this.mensaje.length; j += this.lineas) {
+        resultado += this.mensaje[j];
+      }
+    }
+    this.resultado = resultado;
+  }
+
+  descifrarMensaje(): void {
+    let resultado = '';
+    const longitudLinea = Math.ceil(this.mensaje.length / this.lineas);
+    for (let i = 0; i < longitudLinea; i++) {
+      for (let j = i; j < this.mensaje.length; j += longitudLinea) {
+        resultado += this.mensaje[j];
+      }
+    }
+    this.resultado = resultado;
+  }
 }
